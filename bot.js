189976369bc324ed9217a430011f861a65ce57c7
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-03-23 13:20:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3011,7 +3011,7 @@ client.on("guildMemberAdd", member => {
       var jimp = require('jimp')
       
       const w = ['w.png'
-	                         'w1.png'
+	                         `w1.png`
 				 'w2.png'
 				 'w3.png'
 				 'w4.png'
